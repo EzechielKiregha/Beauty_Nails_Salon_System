096f27380615aa97f25bb8c4cc421f3783ad56d0
EzechielKiregha <ekiregha@gmail.com>
2026-03-06 07:10:30 +0200

    suspense is inevitable

--- app/(auth)/auth/login/page.tsx
+++ app/(auth)/auth/login/page.tsx
@@ -1,5 +1,16 @@
+import LoaderBN from '@/components/Loader-BN';
 import Login from '@/components/pages/Login';
+import { Suspense } from 'react';
+
+export const metadata = {
+  title: 'Login - Beauty Nails',
+};
+
 export default function LoginPage() {
 
-  return <Login />;
+  return (
+    <Suspense fallback={<LoaderBN />}>
+      <Login />;
+    </Suspense>
+  )
 }
--- app/(auth)/auth/signup/page.tsx
+++ app/(auth)/auth/signup/page.tsx
@@ -1,6 +1,17 @@
+import LoaderBN from '@/components/Loader-BN';
 import Signup from '@/components/pages/Signup';
+import { Suspense } from 'react';
+
+export const metadata = {
+  title: 'Register - Beauty Nails',
+};
 
 export default function SignupPage() {
 
-  return <Signup />;
+  return (
+    <Suspense fallback={<LoaderBN />}>
+      <Signup />;
+    </Suspense>
+  )
 }
+
